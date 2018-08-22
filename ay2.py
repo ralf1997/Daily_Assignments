e6ad582d27c9420ebe39be8438fdf9487c28ef7a
ralf1997 <rachitgoelchd@gmail.com>
2018-08-23 04:28:19 +0530

    file modified

--- ay2.py
+++ ay2.py
@@ -1,4 +1,28 @@
+#Q1
+print("Hello I am Rachit Goel");
+
+#Q2
 str1="hello "
 str2="people"
 str3 = str1 + str2
 print (str3)
+
+#Q3
+x = input()
+y = input()
+z = input()
+print (x, " ",y, " ",z)
+
+#Q4
+print ("Let's get started")
+
+#Q5
+s = "Acadviem"
+course= "Python"
+fees= 5000
+print ("%s %s %d"%(s,course,fees))
+
+#Q6
+pi =3.14
+r = int(input())
+print ("area: ",((r*r)*pi))
